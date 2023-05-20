76973945cb1304f913d8dfc21abbd73e4d6c3b38
kishan <kishan@switchon.io>
2023-05-20 21:35:00 +0530

    solved file read and write problem

--- Threading/19_event_exemple2.py
+++ Threading/19_event_exemple2.py
@@ -3,51 +3,44 @@
     This code is written for python version 3.8
 
 '''
+from time import sleep
+from threading import Thread, Event
+from queue import Queue
 
-import threading
-import time
+e = Event()
+e.set()
 
-e = threading.Event()
-
-lines=None
-
-def read():
-    global lines
+def producer(myque):
     n=1
     while True:
-        if not e.is_set():
-            with open('read.txt','r') as fp:
-                print("Reading from file A")        
-                try:    
-                    lines = fp.readlines()[n-1:n+4]
-                    n=n+5
-                except:
-                    lines = fp.readlines()[n:]
-                    print("Here")
-                    e.clear()
-                    return
-            time.sleep(.2)
-            e.set()
-            print(n)
-        
-        
+        with open('read.txt','r') as fp:
+            print("Reading from file A")
+            # try:    
+            lines = fp.readlines()[n-1:n+4]
+            if lines==[]:
+                print("End of file A. No more lines to read")
+                myque.put(None)
+                e.clear()
+                break   
+            myque.put(lines)
+            n=n+5
+            sleep(1)  
+    
 
-def write():
-    e.wait()
+def consumer(myque):
     while e.is_set():
+        lines = myque.get()
+        if not lines:
+            break
         with open('write.txt','a') as fp:
             print("Writing to file B")
             fp.writelines(lines)
-        e.clear()
-        time.sleep(.2)
-        e.wait()
+        
 
-t1 = threading.Thread(target=read)
-t2 = threading.Thread(target=write)
+myque = Queue()
+
+t1 = Thread(target = producer, args=(myque,))
+t2 = Thread(target = consumer, args=(myque,))
 
 t1.start()
 t2.start()
-
-
-# with open('read.txt','w') as fp:
-#     fp.writelines([f"Line {i}\n" for i in range(1,53)])
