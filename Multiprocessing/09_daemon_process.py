e93e97301a4ea27ec33f166525e87cc983636476
kishan <kishan@switchon.io>
2023-06-10 21:05:17 +0530

    modified 09_daemon_process.py

--- Multiprocessing/09_daemon_process.py
+++ Multiprocessing/09_daemon_process.py
@@ -17,20 +17,25 @@ from playsound import playsound
 
 
 def child():
-    print("Child process starts here.")
-    sleep(2)
-    playsound('beep-08b.wav')
-    sleep(2)
-    playsound('beep-08b.wav')
-    print("Child process ends here.")
+    while True:
+        print("Child process starts here.")
+        sleep(2)
+        with open('write.txt','a+') as fp:
+            fp.write('DGH\n')
+        sleep(2)
+        with open('write.txt','a+') as fp:
+            fp.write('DGH22\n')
+        print("Child process ends here.")
 
 if __name__ == "__main__":
     print("Main process starts here.")
     numbers = [2,3,5]
 
     # p = multiprocessing.Process(target=child)
-    p = multiprocessing.Process(target=child, daemon=True)
+    p = multiprocessing.Process(target=child, daemon=False)
 
     p.start()
-    p.join()
+    while True:
+        sleep(1)
+    # p.join()
     print("Main process ends here.")
